3aafad85840445b383f79f8afc8582701fc76a48
bernardo maia <bemaia28@gmail.com>
2021-04-10 13:44:05 -0300

    Adicionar arquivos de projeto.

---
+++ Av3C5Ex05/Form1.Designer.cs
@@ -0,0 +1,86 @@
+﻿
+namespace Av3C5Ex05
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtmes = new System.Windows.Forms.TextBox();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtmes
+            // 
+            this.txtmes.Location = new System.Drawing.Point(58, 62);
+            this.txtmes.Name = "txtmes";
+            this.txtmes.Size = new System.Drawing.Size(118, 20);
+            this.txtmes.TabIndex = 0;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Location = new System.Drawing.Point(61, 88);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(111, 23);
+            this.btnCalcular.TabIndex = 1;
+            this.btnCalcular.Text = "Descobrir o mês";
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(58, 43);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(118, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Digite o número do mês";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(251, 179);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.txtmes);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtmes;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Av3C5Ex05/Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Av3C5Ex05
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            int mes = int.Parse(txtmes.Text);
+            
+            if(mes > 0 && mes < 13)
+            {
+                if(mes == 1)
+                {
+                    MessageBox.Show("Janeiro");
+                }
+                if (mes == 2)
+                {
+                    MessageBox.Show("Fevereiro");
+                }
+                if (mes == 3)
+                {
+                    MessageBox.Show("Março");
+                }
+                if (mes == 4)
+                {
+                    MessageBox.Show("Abril");
+                }
+                if (mes == 5)
+                {
+                    MessageBox.Show("Maio");
+                }
+                if (mes == 6)
+                {
+                    MessageBox.Show("Junho");
+                }
+                if (mes == 7)
+                {
+                    MessageBox.Show("Julho");
+                }
+                if (mes == 8)
+                {
+                    MessageBox.Show("Agosto");
+                }
+                if (mes == 9)
+                {
+                    MessageBox.Show("Setembro");
+                }
+                if (mes == 10)
+                {
+                    MessageBox.Show("Outubro");
+                }
+                if (mes == 11)
+                {
+                    MessageBox.Show("Novembro");
+                }
+                if (mes == 12)
+                {
+                    MessageBox.Show("Dezembro");
+                }
+            }
+            else{ MessageBox.Show("digite o número de um mês valido");}
+            txtmes.Text = "";
+        }
+    }
+}
